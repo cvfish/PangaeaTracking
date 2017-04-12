a31484aa1fbfbf0f56a1270188997a98b47c8c2a
cvfish <ruiyux01@cs.ucl.ac.uk>
2017-04-12 15:09:51 +0100

    update readme

--- examples/Yiwan.sh
+++ examples/Yiwan.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # loading results
-../build/PangaeaTracking/bin/PangaeaTracking ../config/Yiwan_loading.yml
-#../build/PangaeaTracking/bin/PangaeaTracking ../config/Yiwan_loading_new_normals.yml
-#../build_debug/PangaeaTracking/bin/PangaeaTracking ../config/Yiwan_loading.yml
+# # ../build/PangaeaTracking/bin/PangaeaTracking ../config/Yiwan_loading.yml
+# ../build/PangaeaTracking/bin/PangaeaTracking_console ../config/Yiwan_tracking_qi_template.yml
+# #../build/PangaeaTracking/bin/PangaeaTracking ../config/Yiwan_loading_new_normals.yml
+# #../build_debug/PangaeaTracking/bin/PangaeaTracking ../config/Yiwan_loading.yml
 
 # loading results into a buffer first
 #../build_debug/PangaeaTracking/bin/PangaeaTracking ../config/Yiwan_loading_fast.yml
@@ -16,3 +17,11 @@
 #../build/PangaeaTracking/bin/PangaeaTracking ../config/Yiwan_tracking_depthMapInput.yml
 # ../build/PangaeaTracking/bin/PangaeaTracking_console ../config/Yiwan_tracking_depthMapInput.yml
 #../build/PangaeaTracking/bin/PangaeaTracking_console ../config/Tshirt_tracking_depthMapInput.yml
+
+# do rigid tracking
+# ../build/PangaeaTracking/bin/PangaeaTracking_console_bk ../config/Yiwan_rigid_tracking.yml
+# ../build/PangaeaTracking/bin/PangaeaTracking_console ../config/Yiwan_rigid_tracking.yml
+# ../build/PangaeaTracking/bin/PangaeaTracking_console ../config/Yiwan_rigid_tracking_debug.yml
+
+# load rigid tracking results
+../build/PangaeaTracking/bin/PangaeaTracking ../config/Yiwan_loading_rigid.yml
